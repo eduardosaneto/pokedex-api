ac1d7d1d05ce139533b255624e23dab194aef153
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-08-09 15:13:06 -0300

    entidades de pokemons

---
+++ src/entities/Pokemons.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import UsersPokemons from "./UsersPokemons";
+
+@Entity("pokemons")
+export default class Sessions{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    number: number;
+
+    @Column()
+    image: string;
+
+    @Column()
+    weight: number;
+
+    @Column()
+    height: number;
+
+    @Column()
+    baseExp: number;
+
+    @Column()
+    description: string;
+
+    @Column()
+    inMyPokemons: boolean;
+
+    @OneToMany(() => UsersPokemons, pokemons => pokemons.pokemon)
+    myPokemons: UsersPokemons[];
+}
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import UsersPokemons from "./UsersPokemons";
 
 @Entity("users")
 export default class User {
@@ -10,4 +11,7 @@ export default class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => UsersPokemons, (pokemon) => pokemon.user)
+  pokemon: UsersPokemons[]
 }
